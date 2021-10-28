54fe0fe2a2ac102a86ba40648f6afff7b4fab151
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-27 18:15:08 -0700

    Clean working directory with passing test

--- __tests__/tests/simpleDB-test.js
+++ __tests__/tests/simpleDB-test.js
@@ -1,14 +1,22 @@
-const { rm, mkdir } = require('fs/promises');
-const simpleDB = require('../../src/simpleDB');
+const { rm, mkdir, readdir } = require('fs/promises');
+const SimpleDB = require('../../src/simpleDB');
 
 describe('make file', () => {
   const rootDir = './__tests__/objects';
 
   beforeEach(() => {
-    return rm(rootDir, { force: true, recurisive: true }).then(() => {
-      mkdir(rootDir, { recursive: true });
-    });
+    return mkdir(rootDir, { recursive: true });
   });
 
-  it('should make a file in the objects folder', () => {});
+  afterEach(() => {
+    //this is the fs version of rm -rf
+    return rm(rootDir, { force: true, recursive: true });
+  });
+
+  it('should create a new objects directory in __tests__', async () => {
+    const actual = await readdir('./__tests__');
+    const expected = ['objects'];
+
+    expect(actual).toEqual(expect.arrayContaining(expected));
+  });
 });

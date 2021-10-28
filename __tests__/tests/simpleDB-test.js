bfbbb0e065204a088d863ba3601f4d6bbc72436b
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-28 06:55:08 -0700

    Save and get by id with passing test

--- __tests__/tests/simpleDB-test.js
+++ __tests__/tests/simpleDB-test.js
@@ -4,29 +4,26 @@ const SimpleDB = require('../../src/simpleDB');
 describe('make folder', () => {
   const rootDir = './__tests__/objects';
 
-  beforeEach(async () => {
+  beforeEach(() => {
     //this is the fs version of rm -rf
     return rm(rootDir, { force: true, recursive: true });
   });
 
   it('should create a new objects directory in __tests__', () => {
     const expected = ['objects'];
-    new SimpleDB(rootDir)
-      .then(() => readdir('./__tests__'))
-      .then((folders) =>
-        expect(folders).toEqual(expect.arrayContaining(expected))
-      );
+    new SimpleDB(rootDir);
+
+    return readdir('./__tests__').then((folders) =>
+      expect(folders).toEqual(expect.arrayContaining(expected))
+    );
   });
 
   it('.save should take in an object and add a unique id then make a JSON file with that id as the name, then .get should use id to get that file and parse it', () => {
-    const databaseEntry = new SimpleDB(rootDir);
-
-    const objectToSave = { something: 'something' };
-    const expected = { id: expect.any(String), something: 'something' };
-
-    return databaseEntry
-      .save(objectToSave)
-      .then((id) => databaseEntry.get(id))
-      .then((obj) => expect(obj).toEqual(expected));
+    const newDB = new SimpleDB(rootDir);
+    const newObj = { something: 'something' };
+    return newDB
+      .save(newObj)
+      .then(() => newDB.get(newObj.id))
+      .then((results) => expect(results).toEqual(newObj));
   });
 });
--- src/simpleDB.js
+++ src/simpleDB.js
@@ -1,8 +1,33 @@
-const { mkdir } = require('fs/promises');
+const { mkdir, writeFile, readFile } = require('fs/promises');
+const { nanoid } = require('nanoid');
+const path = require('path');
 
 class SimpleDB {
   constructor(rootDir) {
-    return mkdir(rootDir, { force: true, recursive: true });
+    this.rootDir = rootDir;
+    mkdir(rootDir);
+  }
+
+  save(obj) {
+    this.id = nanoid().slice(0, 5);
+    const filename = `${this.id}.json`;
+    const filePath = path.join(this.rootDir, filename);
+
+    obj.id = this.id;
+    const stringyObj = JSON.stringify(obj);
+    return writeFile(filePath, stringyObj);
+  }
+
+  get(id) {
+    const newPath = `${this.rootDir}/${id}.json`;
+    return readFile(newPath, JSON)
+      .then((result) => JSON.parse(result))
+      .catch((err) => {
+        if (err.code === 'ENOENT') {
+          return null;
+        }
+        throw err;
+      });
   }
 }
 

aecd0aea2e3fd64064cf40450a6dac9d0ca551d7
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-27 21:20:16 -0700

    Fourth and final commit on actual working constructor test

--- __tests__/tests/simpleDB-test.js
+++ __tests__/tests/simpleDB-test.js
@@ -1,19 +1,22 @@
 const { rm, readdir } = require('fs/promises');
 const SimpleDB = require('../../src/simpleDB');
 
-describe('make file', () => {
+describe('make folder', () => {
   const rootDir = './__tests__/objects';
 
-  beforeEach(() => {
+  beforeEach(async () => {
     //this is the fs version of rm -rf
     return rm(rootDir, { force: true, recursive: true });
   });
 
-  it('should create a new objects directory in __tests__', async () => {
-    new SimpleDB(rootDir);
-    const actual = await readdir('./__tests__');
+  it('should create a new objects directory in __tests__', () => {
     const expected = ['objects'];
-
-    expect(actual).toEqual(expect.arrayContaining(expected));
+    new SimpleDB(rootDir)
+      .then(() => readdir('./__tests__'))
+      .then((folders) =>
+        expect(folders).toEqual(expect.arrayContaining(expected))
+      );
   });
+
+  // it('should create an object with a unique id with that id as the name of JSON file', ())
 });
